724d914151c26d7005802b19ae04378a686f16b7
LSH <koelkorea@gmail.com>
2025-12-17 03:48:13 +0900

    TEST2

--- src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
+++ src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
@@ -9,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class BackendportfolioApplication {
-    System.out.ptintln ("GIT ACTION TEST!");
     public static void main(String[] args) {
         SpringApplication.run(BackendportfolioApplication.class, args);
     }
